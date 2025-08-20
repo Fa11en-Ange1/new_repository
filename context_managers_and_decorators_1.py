83f2f7d92525ece1d600881cae66aba6592c8548
Fa11en-Ange1 <fallena579@gmail.com>
2025-08-20 22:14:20 +0300

    4-7

--- context_managers_and_decorators_1.py
+++ context_managers_and_decorators_1.py
@@ -1,12 +1,16 @@
 #4
-from contextlib import contextmanager
 import time
 
-@contextmanager
-def timeit():
-    start_time = time.time()
-    yield
-    end_time =  time.time()
-    print(f'Execution time: {end_time - start_time: }')
+def timeit(func):
+    def wrapper():
+        start_time = time.time()
+        result = func()
+        end_time = time.time()
+        print(f"Execution time: {func.__name__}: {end_time - start_time} секунд")
+        return result
+    return wrapper
 
-
+@timeit
+def func_test():
+    time.sleep(2)
+func_test()
