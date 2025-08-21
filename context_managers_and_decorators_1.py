852e25ed8cac871d267547658f02b4392cafad49
Fa11en-Ange1 <fallena579@gmail.com>
2025-08-21 09:37:05 +0300

    4-7

--- context_managers_and_decorators_1.py
+++ context_managers_and_decorators_1.py
@@ -1,16 +1,37 @@
-#4
-import time
+# #4
+# import time
+#
+# def timeit(func):
+#     def wrapper():
+#         start_time = time.time()
+#         result = func()
+#         end_time = time.time()
+#         print(f"Execution time: {func.__name__}: {end_time - start_time} секунд")
+#         return result
+#     return wrapper
+#
+# @timeit
+# def func_test():
+#     time.sleep(2)
+# func_test()
 
-def timeit(func):
-    def wrapper():
-        start_time = time.time()
-        result = func()
-        end_time = time.time()
-        print(f"Execution time: {func.__name__}: {end_time - start_time} секунд")
-        return result
-    return wrapper
+#5
+def retry(n):
+    def decorator(func):
+        def wrapper():
+            for i in range(n):
+                try:
+                    return func()
+                except Exception as e:
+                    print("Error:", e)
+            print("Failed to execute")
+        return wrapper
+    return decorator
 
-@timeit
-def func_test():
-    time.sleep(2)
-func_test()
+
+@retry(3)
+def unstable():
+    print("Processing")
+    raise ValueError("Error")
+
+unstable()
